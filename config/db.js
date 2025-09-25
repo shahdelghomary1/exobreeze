b79f81579a8a747efc546b89aa2a70af11f5695b
shahdelghomary1 <shahdelgomary1@gmail.com>
2025-09-25 04:41:28 +0300

    Remove .env file

--- config/db.js
+++ config/db.js
@@ -2,8 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB Connected");
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB connected successfully");
   } catch (err) {
     console.error("❌ DB Error:", err.message);
     process.exit(1);
--- server.js
+++ server.js
@@ -75,7 +75,7 @@ passport.deserializeUser((id, done) =>
 /* ---------------- ROUTES ---------------- */
 app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
 );
